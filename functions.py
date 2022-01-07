cb72e0c819d734e7577224f91eb3bce81e2353e6
amoscookeh <amosliew1999@gmail.com>
2022-01-07 11:58:39 +0800

    receipt parsing

---
+++ functions.py
@@ -0,0 +1,37 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, InlineQueryHandler, \
+    CallbackQueryHandler, DictPersistence
+
+
+# First time starting the bot
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Welcome to PayMeLah Bot!",
+                             parse_mode='HTML')
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Start the bill splitting process with one of the following commands: "
+                                  + "\n\n/upload - Upload a receipt image to be parsed"
+                                  + "\n/split - Begin bill splitting without receipt parsing")
+
+
+PARSE = range(1)
+
+
+def ask_for_receipt(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Send me image of your receipt",
+                             parse_mode='HTML')
+    return PARSE
+
+
+def parse_receipt(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Send me image of your receipt",
+                             parse_mode='HTML')
+    return ConversationHandler.END
+
+
+def cancel(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Upload cancelled",
+                             parse_mode='HTML')
+    return ConversationHandler.END
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, InlineQueryHandler, \
     CallbackQueryHandler, DictPersistence
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputTextMessageContent
+from functions import start, ask_for_receipt, parse_receipt, cancel
 import os
 import logging
 
@@ -11,17 +12,20 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 
-# First time starting the bot
-def start(update, context):
-    # format_user_data(update, context)
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Welcome to PayMeLah Bot! U+2728",
-                             parse_mode='HTML')
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Start the bill splitting process with one of the following commands: "
-                                  + "\n\n/upload - Upload an image to be parsed"
-                                  + "\n/split - Begin bill splitting without receipt parsing")
-    # return USERNAME
+# Receipt Parsing Conversation
+PARSE = range(1)
+receipt_parsing = ConversationHandler(
+    entry_points=[CommandHandler('upload', ask_for_receipt)],
+    states={
+        PARSE: [
+            MessageHandler(
+                Filters.text & (~Filters.command), parse_receipt
+            )
+        ],
+    },
+    fallbacks=[CommandHandler('cancel', cancel)],
+    conversation_timeout=300
+)
 
 
 def main():
@@ -29,7 +33,7 @@ def main():
     dispatcher = updater.dispatcher
 
     # dispatcher.add_handler(registration_handler)
-    # dispatcher.add_handler(conv_handler)
+    dispatcher.add_handler(receipt_parsing)
     # dispatcher.add_handler(InlineQueryHandler(inlinequery))
     # dispatcher.add_handler(CallbackQueryHandler(callbackhandle))
     # dispatcher.add_handler(CommandHandler('help', help))
