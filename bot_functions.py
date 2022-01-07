6e0371e5cf953b6e3223bb6b59c825294ac81b12
amoscookeh <amosliew1999@gmail.com>
2022-01-07 17:28:20 +0800

    Countdown

--- bot_functions.py
+++ bot_functions.py
@@ -11,6 +11,10 @@ TABSCANNER_TOKEN = os.environ['TABSCANNER_TOKEN']
 WEBAPP_LINK = 'http://www.google.com/split'
 
 money_emoji = '\U0001F4B8'
+receipt_emoji = '\U0001F9FE'
+divide_emoji = '\U00002797'
+sad_shocked_emoji = '\U0001F616'
+hourglass_emoji = '\U0000231B'
 
 # First time starting the bot
 def start(update, context):
@@ -19,17 +23,16 @@ def start(update, context):
                                                                                money_emoji, money_emoji))
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text="Start the bill splitting process with one of the following commands: "
-                                  + "\n\n/upload - Upload a receipt image to be parsed"
-                                  + "\n/split - Begin bill splitting without receipt parsing")
-
+                                  + "\n\n/upload {} - Upload a receipt image to be parsed"
+                                  + "\n/split {} - Begin bill splitting without receipt parsing").format(receipt_emoji, divide_emoji)
+    countdown(update, context, 5)
 
 PARSE = range(1)
 
 
 def ask_for_receipt(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Send me image of your receipt",
-                             parse_mode='HTML')
+                             text="Please send me a clear image of your {} Receipt {}".format(receipt_emoji, receipt_emoji))
     return PARSE
 
 
@@ -37,9 +40,9 @@ def parse_receipt(update, context):
     receipt_photo = update.message.photo[-1]
     file_id = receipt_photo.file_id
     photo = context.bot.get_file(file_id)
-    photo_file = photo.download("{}.jpg".format(file_id))
+    photo.download("{}.jpg".format(file_id))
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Parsing receipt",
+                             text="The Robots have begun parsing your receipt...",
                              parse_mode='HTML')
 
     output = callProcess(TABSCANNER_TOKEN, "{}.jpg".format(file_id))
@@ -53,7 +56,7 @@ def parse_receipt(update, context):
         result = callResult(TABSCANNER_TOKEN, output_token)
     if (result['status'] == 'failed'):
         context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="Image parsing failed",
+                                 text="Image parsing failed {}".format(sad_shocked_emoji),
                                  parse_mode='HTML')
     else:
         data = {
@@ -77,3 +80,16 @@ def cancel(update, context):
                              text="Upload cancelled",
                              parse_mode='HTML')
     return ConversationHandler.END
+
+
+def countdown(update, context, seconds):
+    timer = seconds
+    sent_message = context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="{} seconds remaining...".format(timer),
+                             parse_mode='HTML')
+    message_id = sent_message.message_id
+    while (timer > 0):
+        time.sleep(1)
+        timer -= 1
+        context.bot.edit_message_text("{} seconds remaining...".format(timer), message_id=message_id)
+    context.bot.delete_message(chat_id=update.effective_chat.id, message_id=message_id)
