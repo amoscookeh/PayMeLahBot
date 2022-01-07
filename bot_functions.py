0c9df77710b4d8ee610558c5e2466d79e82087b8
amoscookeh <amosliew1999@gmail.com>
2022-01-07 19:14:30 +0800

    change the link

--- bot_functions.py
+++ bot_functions.py
@@ -8,7 +8,7 @@ from tabscanner_functions import callProcess, callResult
 from helper_functions import format_line_items
 
 TABSCANNER_TOKEN = os.environ['TABSCANNER_TOKEN']
-WEBAPP_LINK = 'http://www.google.com/split'
+WEBAPP_LINK = 'https://paymelah.vercel.app/split'
 
 money_emoji = '\U0001F4B8'
 receipt_emoji = '\U0001F9FE'
